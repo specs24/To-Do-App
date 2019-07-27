a8c143b698a18ae3fdb1f9a96e0392e159a52d3d
Wendy <33762861+specs24@users.noreply.github.com>
2019-07-27 10:40:01 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+function onReady() {
+	const addToDoForm = document.getElementById('addToDoForm');
+	const newToDoText = document.getElementById('newToDoText');
+	const ToDoList = document.getElementById('ToDoList');
+}
+
+addToDoForm.addEventListener('submit',() =>{
+	event.preventdefault();
+	let title = newToDoText.value;
+});
+}
+
+
 window.onload = function() {
 	alert("The window has loaded");
+	onReady();
 };
+
+	

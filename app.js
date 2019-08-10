b542cd29d85edb342a04d273e749e9b4eec3ed99
Wendy <33762861+specs24@users.noreply.github.com>
2019-08-10 11:24:25 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 function on ready() {
-	let toDos = [];	
 	const addToDoForm = document.getElementById('addToDoForm');
+		
+	let toDos = [];
 
-	function renderTheUI() {
+	function renderTheUI(){
 		const toDoList = document.getElementById('toDoList');
-
-		toDos.forEach(function(toDo) {
-			const newLi = document.createElement('li');
+		
+		toDos.forEach(function(toDo){
+			const newLi document.createElement('li');
 			const checkbox = document.createElement('input');
 			checkbox.type = "checkbox";
 
 			newLi.textContent = toDo.title;
 			newLi.appendChild(checkbox);
+
 			toDoList.appendChild(newLi);
-			
-			newToDoText.value = '';
-
-		});
-	}
-
-	function createNewToDo(){
-		const newToDoText = document.getElementById('newToDoText');
+		})
+	
+}
+	
+	function createNewToDo() {
+		const newToDoText = document.getElementByID('newToDoText');
 		toDos.push({
 			title: newToDoText.value,
 			complete: false
-		});
-	
-		newToDoText.value = '';
-		renderTheUI();
+		});	
+	newToDoText.value = '';
+	renderTheUI();
 	}
 
-	addToDoForm.addEventListener('submit',event => {
-		event.preventdefault();
+	addToDoForm.addEventListener('submit', event=>{
+		event.preventDefault();
 		createNewToDo();
 	});
 
 	renderTheUI();
 }
+	
 
 window.onload = function() {
 	onready();

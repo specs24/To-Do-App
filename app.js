d95da91f48bc5d0ea401a06d27a5e9cde56b3470
Wendy <33762861+specs24@users.noreply.github.com>
2019-08-13 01:18:46 -0400

    JS Update

--- app.js
+++ app.js
@@ -2,34 +2,52 @@ function onReady() {
 	const addToDoForm = document.getElementById('addToDoForm');
 		
 	let toDos = [];
+	let id = 0;
 
 	function renderTheUI(){
 		const toDoList = document.getElementById('toDoList');
-		
+		toDoList.textContent = '';
+
 		toDos.forEach(function(toDo){
 			const newLi = document.createElement('li');
 			const checkbox = document.createElement('input');
 			checkbox.type = "checkbox";
 
-			newLi.textContent = toDo.title;
-			newLi.appendChild(checkbox);
+			const deleteBtn = document.createElement('button');
+			deleteBtn.textContent = "Delete!";
 
+			deleteBtn.addEventListener('click', event => {
+				toDos = toDos.filter(function(item){
+					return item, id !== toDo.id;
+				renderTheUI();
+			});
+
+			newLi.textContent = toDo.title;
+			
 			toDoList.appendChild(newLi);
+			newLi.appendChild(checkbox);
+			newLi.appendChild(deleteBtn);
+			
 		})
 	
 }
 	
 	function createNewToDo() {
 		const newToDoText = document.getElementById('newToDoText');
+		if(!newToDoText.value){return;}
 		toDos.push({
 			title: newToDoText.value,
-			complete: false
+			complete: false.
+			id: id
 		});	
+
+		id++;
+
 	newToDoText.value = '';
 	renderTheUI();
 	}
 
-	addToDoForm.addEventListener('submit', event=>{
+	addToDoForm.addEventListener('submit', function(event){
 		event.preventDefault();
 		createNewToDo();
 	});
@@ -39,8 +57,5 @@ function onReady() {
 	
 
 window.onload = function() {
-	onready();
+	onReady();
 };
-
-	
-   		
